72e299684e9121c385560da13ebe4f4a6a2a0dfe
Sunghoon Chung <holdofhim@gmail.com>
2015-10-30 15:32:03 +0900

    20150717

--- China_M_structure.R
+++ China_M_structure.R
@@ -4,7 +4,7 @@
 
 rm(list = ls())                        # Remove all
 setwd("D:/Copy/GVC/ICIO/Rcode")        # Working Directory
-rawd  <- "D:/Copy/GVC/ICIO/"           # Excel Raw data Directory
+excel  <- "D:/Copy/GVC/ICIO/excel/"     # Excel Raw data Directory
 rdata <- "D:/Copy/GVC/ICIO/Rdata/"     # RData Saving Directory
 
 library(foreign)
@@ -12,7 +12,7 @@ library(matlab)
 library(xlsx)
 
 ## ICIO includes 62 countries (plus 5 processing ID) with 34 industries over 1995-2011
-period <- c(1995,2000,2005,2010,2011)
+period <- c(1995,2000,2005,2008,2010,2011)
 
 ## Some Countries by Sectors
 chn <- c(1309:1377)
@@ -26,7 +26,9 @@ for (yr in period) {
       M.chn.kor <- colSums(M.chn[kor,])
       M.chn.jpn <- colSums(M.chn[jpn,])
       M.chn.row <- colSums(M.chn)-M.chn.chn-M.chn.kor-M.chn.jpn
-      M.chn.agg <- rbind(M.chn.row, M.chn.jpn, M.chn.chn, M.chn.kor) / (ones(4,1)%*%colSums(M.chn)) * 100
-      M.chn.agg <- cbind(yr, M.chn.agg)
+      M.chn.agg <- rbind(M.chn.chn, M.chn.row, M.chn.jpn, M.chn.kor) / (ones(4,1)%*%colSums(M.chn)) * 100
+      M.chn.agg <- cbind(year=yr,cid=c("CHN","ROW","JPN","KOR"), M.chn.agg)
       if (yr==1995) M.china <- M.chn.agg   else M.china <- rbind(M.china, M.chn.agg)
 }
+
+write.xlsx(M.china, paste0(excel,"China_M_structure.xlsx"), row.names=FALSE)
